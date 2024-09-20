b55d9370179d46f12473ea62b359cf78e82fb551
Ryan Michal <ryan.michal@mutuallyhuman.com>
2024-09-20 09:14:27 -0400

    demo

---
+++ emails/verification-example.tsx
@@ -0,0 +1,170 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface asdfVerifyEmailProps {
+  verificationCode?: string;
+}
+
+const randomImage = `https://picsum.photos/200/300`
+
+export default function AWSVerifyEmail({
+  verificationCode = "596853",
+}: asdfVerifyEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>asdf Email Verification</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={coverSection}>
+            <Section style={imageSection}>
+              <Img
+                src={`${randomImage}`}
+                width="75"
+                height="45"
+                alt="asdf's Logo"
+              />
+            </Section>
+            <Section style={upperSection}>
+              <Heading style={h1}>Verify your email address</Heading>
+              <Text style={mainText}>
+                Thanks we really like you.
+                but we need a code from you so that you are very annoyed with logging in.
+                Please enter the following
+                verification code when prompted. If you don&apos;t we will find you
+              </Text>
+              <Section style={verificationSection}>
+                <Text style={verifyText}>Verification code</Text>
+
+                <Text style={codeText}>{verificationCode}</Text>
+                <Text style={validityText}>
+                  (This code is valid for 10 minutes)
+                </Text>
+              </Section>
+            </Section>
+            <Hr />
+            <Section style={lowerSection}>
+              <Text style={cautionText}>
+                asdf Web Services will never email you and ask you to disclose
+                or verify your password, credit card, or banking account number.
+              </Text>
+            </Section>
+          </Section>
+          <Text style={footerText}>
+            This message was produced and distributed by asdf Web Services,
+            Inc., 410 Terry Ave. North, Seattle, WA 98109. © 2022, asdf Web
+            Services, Inc.. All rights reserved. AWS is a registered trademark
+            of{" "}
+            <Link href="https://asdf.com" target="_blank" style={link}>
+              asdf.com
+            </Link>
+            , Inc. View our{" "}
+            <Link href="https://asdf.com" target="_blank" style={link}>
+              privacy policy
+            </Link>
+            .
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
+
+const main = {
+  backgroundColor: "#fff",
+  color: "#212121",
+};
+
+const container = {
+  padding: "20px",
+  margin: "0 auto",
+  backgroundColor: "#eee",
+};
+
+const h1 = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "20px",
+  fontWeight: "bold",
+  marginBottom: "15px",
+};
+
+const link = {
+  color: "#2754C5",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  textDecoration: "underline",
+};
+
+const text = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  margin: "24px 0",
+};
+
+const imageSection = {
+  backgroundColor: "#252f3d",
+  display: "flex",
+  padding: "20px 0",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const coverSection = { backgroundColor: "#fff" };
+
+const upperSection = { padding: "25px 35px" };
+
+const lowerSection = { padding: "25px 35px" };
+
+const footerText = {
+  ...text,
+  fontSize: "12px",
+  padding: "0 20px",
+};
+
+const verifyText = {
+  ...text,
+  margin: 0,
+  fontWeight: "bold",
+  textAlign: "center" as const,
+};
+
+const codeText = {
+  ...text,
+  fontWeight: "bold",
+  fontSize: "36px",
+  margin: "10px 0",
+  textAlign: "center" as const,
+};
+
+const validityText = {
+  ...text,
+  margin: "0px",
+  textAlign: "center" as const,
+};
+
+const verificationSection = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const mainText = { ...text, marginBottom: "14px" };
+
+const cautionText = { ...text, margin: "0px" };
